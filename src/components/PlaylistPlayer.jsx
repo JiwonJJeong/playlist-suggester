7f603e6eeb5bdb14e55a8e6afbe0433f88a179c8
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-07-11 23:53:43 -0700

    Feat: Setup error and load catching for API fetch
    Need to fetch from API in PlaylistPlayer since
    we need to create a playlist from given inputs

--- src/components/PlaylistPlayer.jsx
+++ src/components/PlaylistPlayer.jsx
@@ -1,4 +1,24 @@
+import {useState, useEffect} from "react";
+
 export default function PlaylistPlayer ({selectedInputsRef}){
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(()=>{
+        fetch("PUT API LINK HERE", {mode: "cors"})
+            .then((response)=> {
+                if (response.status >= 400) {throw new Error("server error")}
+                return response.json();
+            })
+            .then((response) => /*add stuff here based on what we get back*/ true)
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    }, []);
+
+    if (loading) return <p>Loading...</p>
+    if (error) return <p>A network error occured.</p>
+
+
     return (
         <>
         <h1>hi! This is where the playlist player should go after playlist is produced</h1>
