ab37cbc5ae2d977128b78cffd012f3a4e860be03
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-07-11 23:24:32 -0700

    Feat: Create playlist player page and swap logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,16 @@
 import DiskReceiver from "./DiskReceiver.jsx"
+import PlaylistPlayer from  "./PlaylistPlayer.jsx"
 import Disk from "./Disk.jsx"
 import '../styles/App.css'
 import {useRef, useState} from "react";
+import {Link} from "react-router-dom";
+
 
 function App() {
   // Stores an array of input strings like ["90s","Beyonce"]
   // later maybe separate strings by type (decade, artist, genre)
   const selectedInputsRef = useRef([]);
+  const [isFinalized, setFinalized] = useState(false);
 
   function addSelectedInput(input){
     selectedInputsRef.current = [...selectedInputsRef.current, input];
@@ -20,10 +24,16 @@ function App() {
 
   return (
     <>
-      {diskStrings.map((string) => {
-        return <Disk inputText={string}></Disk>
-      })}
-      <DiskReceiver addSelectedInput={addSelectedInput} />
+      {(!isFinalized) ? <>
+        {diskStrings.map((string) => {
+          return <Disk inputText={string}></Disk>
+        })}
+        <DiskReceiver addSelectedInput={addSelectedInput} />
+        <button onClick={()=>setFinalized(true)} >Let's make the playlist!</button>
+        </>
+      :
+      <PlaylistPlayer selectedInputsRef={selectedInputsRef} />
+      }
     </>
   )
 }
---
+++ src/components/PlaylistPlayer.jsx
@@ -0,0 +1,8 @@
+export default function PlaylistPlayer ({selectedInputsRef}){
+    return (
+        <>
+        <h1>hi! This is where the playlist player should go after playlist is produced</h1>
+        <p>The selected inputs are... {selectedInputsRef.current}</p>
+        </>
+    )
+}
