f5dc1a85c28fff6783bc2f08623895eaf04d1f81
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-07-11 18:52:46 -0700

    Feat: Create Disk component with text prop

---
+++ src/Disk.jsx
@@ -0,0 +1,11 @@
+/*import "./Disk.css"*/
+
+export default function Disk({inputText}){
+
+    return (
+        <div className="disk">
+            <img></img>
+            <caption>{inputText}</caption>
+        </div>
+    )
+}
