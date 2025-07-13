409b27f8c1035815ce4f36cb44feb13d433d03e7
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2025-07-13 02:15:32 -0700

    Fix: Clean up unused components and styles
    Remove old logic from first iteration where
    Disks and drag and drop API were used

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,12 @@
 import { Routes, Route, Link, useLocation } from "react-router-dom";
 import PlaylistMaker from "./PlaylistMaker.jsx";
-import PlaylistPlayer from "./PlaylistPlayer.jsx";
 import LyricsAssistant from "./LyricsAssistant.jsx";
 import LandingPage from  "./LandingPage.jsx"
 import '../styles/App.css';
-import { useRef, useState } from "react";
 
 function App() {
-  const selectedInputsRef = useRef([]);
-  const [isFinalized, setFinalized] = useState(false);
-  const [diskStrings, setDiskStrings] = useState(["90s", "Pop", "Classical", "Beyonce", "2000s"]);
   const location = useLocation();
 
-  function addSelectedInput(input) {
-    selectedInputsRef.current = [...selectedInputsRef.current, input];
-    setDiskStrings(diskStrings.filter((string) => string !== input));
-    console.log(selectedInputsRef);
-  }
 
   return (
     <div className="app-container">
@@ -51,15 +41,7 @@ function App() {
           <Route
             path="/playlist"
             element={
-              !isFinalized ? (
-                <PlaylistMaker
-                  diskStrings={diskStrings}
-                  addSelectedInput={addSelectedInput}
-                  setFinalized={setFinalized}
-                />
-              ) : (
-                <PlaylistPlayer selectedInputsRef={selectedInputsRef} />
-              )
+                <PlaylistMaker/>
             }
           />
           <Route path="/validate" element={<LyricsAssistant />} />
